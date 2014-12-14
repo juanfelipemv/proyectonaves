120b2301310df2db529d09d65a5be7f61ae35f34
josephfuentes <josephfuentes@unitec.edu>
2014-12-13 19:21:07 -0600

    Disparos y Menus arreglados

--- juego/main.cpp
+++ juego/main.cpp
@@ -1,15 +1,22 @@
 #include <allegro.h>
 #include "inicia.h"
 
+const int maxdisp = 5 , ANCHO = 500, ALTO = 450;
 
+void juego();
+void pintar_nave(BITMAP *nave,BITMAP *buffer,struct NAVE nav);
 
-
-volatile int speed_counter = 0;
 struct NAVE{
     int x,y;
     int dir;
-}nav = {250,300,1};
+    int ndisparos;
+}nav = {250,300,1,0};
 
+struct Balas{
+    int x,y;
+    int dx,dy; //incrementos de las variables  x,y
+
+}disparos[maxdisp];
 
 void cargarInstruccion()
 {
@@ -19,23 +26,92 @@ void cargarInstruccion()
                         blit(menu,buffer,0,0,0,0,500,450);
                         blit(buffer,screen,0,0,0,0,500,450);
                         rest(5);
+                        if(key[KEY_BACKSPACE]) {
+                        break;
+                        }
+
                          }
 
 }
 
-void cargarmenu()
+void juego()
 {
-    while(!key[KEY_ESC]){
-                        BITMAP *buffer = create_bitmap(500,450);
-                        BITMAP *menu = load_tga("menu3.tga",NULL);
-                        blit(menu,buffer,0,0,0,0,500,450);
-                        blit(buffer,screen,0,0,0,0,500,450);
-                        rest(5);
+    BITMAP *nave = load_bitmap("nave.bmp",NULL);
+    BITMAP *nube = load_tga("Galaxy2.tga",NULL);
+     BITMAP *asteroide = load_bitmap("asteroid.bmp",NULL);
+    BITMAP *bala = load_bitmap("Bala2.bmp", NULL);  // otra cosa
+    BITMAP *buffer = create_bitmap(500,450);
 
-                         if(key[KEY_2]) {
-      cargarInstruccion();
+    int i = 450 , dsw = 0 , contt = 0;
+    while(!key[KEY_ESC]){
+        blit(nube,buffer,0,--i,0,0,500,450);  if(i == 0) i=450;
+        pintar_nave(nave,buffer,nav);
+        if(nav.x>0 && nav.x<450){
+       if(key[KEY_UP])        { nav.dir = 1; nav.y -= 1; }
+       else if(key[KEY_DOWN]) { nav.dir = 1; nav.y += 2; }
+       if(key[KEY_RIGHT])     { nav.dir = 2; nav.x += 1; }
+       else if(key[KEY_LEFT]) { nav.dir = 0; nav.x -= 1; }
+       else nav.dir = 1;
+        }else if(nav.x>=450){
+        nav.x=nav.x-5;
+        }else if(nav.x<=0){
+        nav.x=nav.x+5;
+        }
+       ///rutina disparos///
+
+       if(key[KEY_SPACE]&&dsw==0){
+            if(nav.ndisparos<maxdisp){
+                nav.ndisparos++;
+                disparos[nav.ndisparos].x = nav.x + 20;
+                disparos[nav.ndisparos].y = nav.y;
+                disparos[nav.ndisparos].dx = 0;
+                disparos[nav.ndisparos].dy = -3;
+                dsw = 1;
+
+            }
+
+
        }
-                         }
+
+        if(contt++ >20){
+                dsw=0;
+                contt=0;
+            }
+
+
+       if(nav.ndisparos>0){
+        for(int cont=1;cont<=nav.ndisparos;cont++){
+            disparos[cont].x += disparos[cont].dx;
+            disparos[cont].y += disparos[cont].dy;
+            masked_blit(bala,buffer,0,0, disparos[cont].x,disparos[cont].y,4,7);
+
+            if(disparos[cont].y>ALTO || disparos[cont].y <0 ||
+                disparos[cont].x>ANCHO || disparos[cont].x<0){
+
+                    disparos[cont]=disparos[nav.ndisparos];
+                    nav.ndisparos--;
+                    if(nav.ndisparos<0){
+                        nav.ndisparos =0;
+                    }
+                }
+        }
+
+       }
+
+       blit(buffer,screen,0,0,0,0,500,450);
+       rest(5);
+
+    }
+
+    destroy_bitmap(buffer);
+    destroy_bitmap(nave);
+    destroy_bitmap(nube);
+
+
+
+
+
+
 
 }
 
@@ -43,6 +119,7 @@ void cargarmenu()
 
 void cargarVictoria()
 {
+
     while(!key[KEY_ESC]){
                         BITMAP *buffer = create_bitmap(500,450);
                         BITMAP *menu = load_tga("win.tga",NULL);
@@ -68,65 +145,41 @@ void cargarGameover()
 }
 
 
+
 void pintar_nave(BITMAP *nave,BITMAP *buffer,struct NAVE nav){
      masked_blit( nave, buffer, 40*nav.dir, 0, nav.x, nav.y, 40, 47);
 
 }
-void inc_speed_counter()
-{
-    speed_counter++;
-}
-END_OF_FUNCTION(inc_speed_conuter)
+
+
+
 int main()
 {
-    inicia_allegro(500,450);
+     inicia_allegro(500,450);
     inicia_audio(70,70);
 
-    BITMAP *nave = load_bitmap("nave.bmp",NULL);
-    BITMAP *nube = load_tga("Galaxy2.tga",NULL);
-    BITMAP *bala = load_bitmap("Bala2.bmp", NULL);  // otra cosa
-    BITMAP *buffer = create_bitmap(500,450);
 
-    LOCK_VARIABLE(speed_counter);
-    LOCK_FUNCTION(inc_speed_counter);
+    //menu
+     while(!key[KEY_ESC]){
+                        BITMAP *buffer = create_bitmap(500,450);
+                        BITMAP *menu = load_tga("menu3.tga",NULL);
+                        blit(menu,buffer,0,0,0,0,500,450);
+                        blit(buffer,screen,0,0,0,0,500,450);
+                        rest(5);
 
-    install_int_ex(inc_speed_counter, BPS_TO_TIMER(30));
+                         if(key[KEY_2]) {
+                        cargarInstruccion();
+                        }
+                         if(key[KEY_1]) {
+                        juego();
+                        }
+                         if(key[KEY_4]) {
 
-    int i = 450 , dsw = 0 , cont = 0;
-    //cargarmenu()
-    while(!key[KEY_ESC]){
-        blit(nube,buffer,0,--i,0,0,500,450);
-        if(i == 0)
-        i=450;
-        pintar_nave(nave,buffer,nav);
+                       return 0;
+                        }
+                        }
 
-       if(key[KEY_UP]){
-       nav.dir = 1;
-       nav.y -= 1;
-       }
-       else if(key[KEY_DOWN]) {
-       nav.dir = 1;
-       nav.y += 2;
-       }
-       if(key[KEY_RIGHT]){
-       nav.dir = 2;
-       nav.x += 1;
-       }
-       else if(key[KEY_LEFT]){
-       nav.dir = 0;
-        nav.x -= 1;
-        }
-       else nav.dir = 1;
 
-       blit(buffer,screen,0,0,0,0,500,450);
-       rest(5);
-
-    }
-
-    destroy_bitmap(buffer);
-    destroy_bitmap(nave);
-    destroy_bitmap(nube);
-
-    return 0;
 }
 END_OF_MAIN();
+
